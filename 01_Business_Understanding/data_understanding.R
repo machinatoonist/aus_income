2d0292f644b62cd03484a3513d715ec6c2db6bbf
machinatoonist <matt@machinatoonist.com>
2020-12-20 17:34:17 +1000

    Updated plots to include analysis of age
    Created cleaned age strings, age factors and updated theme.

--- 01_Business_Understanding/data_understanding.R
+++ 01_Business_Understanding/data_understanding.R
@@ -6,7 +6,7 @@ library(tidyquant)
 library(readxl)
 library(stringr)
 
-# Load Data
+# Load Data ----
 
 path_train <- "00_Data/ts18individual14occupationsextaxableincomerange.xlsx"
 train_raw_tbl <- read_excel(path_train, sheet = "Individual Table 14A", skip = 3)
@@ -17,7 +17,7 @@ age_raw_tbl <- read_excel(path_age, sheet = "Individuals Table 13", skip = 2)
 glimpse(train_raw_tbl)
 glimpse(age_raw_tbl)
 
-# Clean Data
+# Clean Data ----
 trim_age_tbl <- age_raw_tbl %>%
   rename(
     age_range = `Age range3`,
@@ -43,18 +43,7 @@ trim_age_tbl <- age_raw_tbl %>%
     salary_tot_au,
     salary_tot_nobs,
     tax_bracket
-  ) %>%
-  mutate(
-    ave_salary = salary_tot_au/salary_tot_nobs,
-    ave_taxable_income = taxable_income_au/taxable_income_obs,
-    ave_total_income = income_tot_au/income_tot_nobs
-  ) %>%
-  select(- c(taxable_income_obs,
-         taxable_income_au,
-         income_tot_au,
-         income_tot_nobs,
-         salary_tot_au,
-         salary_tot_nobs))
+  ) 
 
 glimpse(trim_age_tbl)
 
@@ -84,49 +73,54 @@ trim_tbl <- train_raw_tbl %>%
     tax_bracket
   ) 
 
-# %>% View()
-#   mutate(
-#     ave_salary = salary_tot_au/salary_tot_nobs,
-#     ave_taxable_income = taxable_income_au/taxable_income_obs,
-#     ave_total_income = income_tot_au/income_tot_nobs
-#   ) %>%
-#   select(- c(taxable_income_obs,
-#              taxable_income_au,
-#              income_tot_au,
-#              income_tot_nobs,
-#              salary_tot_au,
-#              salary_tot_nobs))
-
 glimpse(trim_tbl)
 
 # 1. Prepare Data Tables ----
 
 summary_tbl_120 <- trim_tbl %>%
   group_by(occupation_group, Sex) %>%
-  summarize(n = n(),
-            individuals = sum(individuals_obs),
+  summarize(individuals = sum(individuals_obs),
             ave_taxable_income = sum(taxable_income_au)/sum(taxable_income_obs),
             ave_total_income = sum(income_tot_au)/sum(income_tot_nobs),
             ave_salary = sum(salary_tot_au)/sum(salary_tot_nobs)
             ) %>%
-  filter(ave_taxable_income > 120000
-         # ,individuals > 500
-         # ,occupation_group != "3242 Vehicle body builder or trimmer"
-         ) %>%
+  filter(ave_taxable_income > 120000) %>%
   arrange(desc(ave_taxable_income)) %>% 
   mutate(
        occupation_group = str_remove_all(occupation_group, pattern = "[0-9]"),
        n_text = str_c(format(individuals / 1e3, digits = 2), "k", sep = "")) 
 summary_tbl_120 %>% glimpse()
 
-sort_by_age <- trim_age_tbl %>%
+summary_tbl_all <- trim_tbl %>%
+  group_by(occupation_group, Sex) %>%
+  summarize(individuals = sum(individuals_obs),
+            ave_taxable_income = sum(taxable_income_au)/sum(taxable_income_obs),
+            ave_total_income = sum(income_tot_au)/sum(income_tot_nobs),
+            ave_salary = sum(salary_tot_au)/sum(salary_tot_nobs)
+  ) %>%
+  arrange(desc(ave_taxable_income)) %>% 
+  mutate(
+    occupation_group = str_remove_all(occupation_group, pattern = "[0-9]"),
+    n_text = str_c(format(individuals / 1e3, digits = 2), "k", sep = "")) 
+summary_tbl_low %>% glimpse()
+
+summary_by_age_tbl <- trim_age_tbl %>%
   filter(residency != "Non-resident") %>%
   group_by(age_range, Sex) %>%
-  summarize(n = n(),
-            ave_taxable = mean(ave_taxable_income),
-            individuals = sum(individuals_obs)) %>%
-  arrange(desc(ave_taxable)) 
-sort_by_age %>% View()
+  summarize(individuals = sum(individuals_obs),
+            ave_taxable_income = sum(taxable_income_au)/sum(taxable_income_obs),
+            ave_total_income = sum(income_tot_au)/sum(income_tot_nobs),
+            ave_salary = sum(salary_tot_au)/sum(salary_tot_nobs)
+  ) %>%
+  arrange(desc(ave_taxable_income)) %>% 
+  mutate(
+    age_range = str_remove_all(age_range, pattern = "[a-z]."),
+    age_range = str_replace_all(age_range, pattern = "75", replacement = "75 +"),
+    age_range = as_factor(age_range),
+    Sex = as_factor(Sex),
+    n_text = str_c(format(individuals / 1e3, digits = 2), "k", sep = "")) 
+summary_by_age_tbl %>% glimpse()
+summary_by_age_tbl %>% View()
 
   
 # Visualisation of Income ----
@@ -144,7 +138,7 @@ summary_tbl_120 %>%
        subtitle = "Grouped by Occupation - Sorted From Highest to Lowest",
        y = "Total Salary $A", x = "Occupation Group") +
   theme(legend.position = c(.8, .8)) +
-  guides(x = guide_axis(angle = 90))
+  guides(x = guide_axis(angle = 45))
   
 summary_tbl_120 %>%
   ggplot(aes(x = occupation_group, y = ave_taxable_income, fill = Sex)) + 
@@ -157,7 +151,31 @@ summary_tbl_120 %>%
        subtitle = "Grouped by Occupation - Sorted From Highest to Lowest",
        y = "Taxable Income - $A", x = "Occupation Group") +
   theme(legend.position = c(.8, .8)) +
-  guides(x = guide_axis(angle = 90))
+  guides(x = guide_axis(angle = 45))
 
+summary_tbl_all %>%
+  ggplot(aes(x = occupation_group, y = ave_taxable_income, fill = Sex)) + 
+  aes(reorder(occupation_group,ave_taxable_income), ave_taxable_income) +
+  geom_col(position = "dodge2") +
+  scale_y_continuous(labels = scales::dollar) +
+  theme_tq() +
+  scale_size(range = c(3,5)) +
+  labs(title = "Average Taxable Income For Individuals in Australia - 2017-18",
+       subtitle = "Grouped by Occupation - Sorted From Lowest to Highest",
+       y = "Taxable Income - $A", x = "") +
+  theme(legend.position = c(.8, .8)) 
+
+summary_by_age_tbl %>%
+  ggplot(aes(x = age_range, y = ave_taxable_income, fill = Sex)) + 
+  aes(reorder(age_range,desc(ave_taxable_income)), ave_taxable_income) +
+  geom_col(position = "dodge2") +
+  scale_y_continuous(labels = scales::dollar) +
+  theme_tq() +
+  scale_size(range = c(3,5)) +
+  scale_fill_tq() + 
+  labs(title = "Average Taxable Income For Individuals in Australia - 2017-18",
+       subtitle = "Grouped by Age Range - Sorted From Highest to Lowest",
+       y = "Taxable Income - $A", x = "Age Group Range") +
+  theme(legend.position = c(.8, .8)) 
 
   
